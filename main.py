5b27474011736e5e7c1065d8d2a2a118f768e332
Ben Neighbour <45554944+BenNeighbour@users.noreply.github.com>
2020-11-19 18:52:03 +0000

    Slightly optimizing/commenting the program
    Added a few notes and stuff here and there. I haven't edited on my machine - just directly on Github - just tell me if it doesn't run on your machine or repl!

--- main.py
+++ main.py
@@ -1,38 +1,48 @@
+# Global username variable
 username = "username"
+
+# Global password variable
 password = "password"
 
-print("===================")
-print("Welcome to the Grade Calculator.")
-print("===================")
 usernameAttempt = input("Please enter your username: ")
 passwordAttempt = input("Please enter your password: ")
 classCode = input("Enter the class code: ")
+
 f = open(classCode, "w+")
 
-
+# Step 1 function
 def step1():
+    
+    # Fix this. I don't know what your conditions for this are but it doesn't look quite right and could be 
+    # improved... But don't worry about it - just trust the process!
     while "no" or " " == end:
-
+        # Prompt for user to enter grades
         print('Please enter your grades in the following format - FirstName-LastName-Grade,')
-        x: str = input('Please enter your grades using the above format. ')
-        f.write(x)
+        grades = str(input('Please enter your grades using the above format. '))
+        
+        # NOTE/TODO: What happens if they don't enter their grades in the above format? I'd validate that here:
+        # .....
+        
+        f.write(grades)
         f.write("\n")
-        done = input("Are you finished? (Y/N) ")
-        if done == "n" or done == "N":
-            end = 'no'
-        if done == "Y" or done == "y":
-            end = 'yes'
+        
+        isDone = bool(input("Are you finished? "))
+        
+        
+        if !isDone:
+            step1()
         else:
-            end = 'no'
-
-        if end == 'yes':
             print("Thank you! Your grades have been recorded!")
             exit()
-        if end == 'no':
-            step1()
+            
+# Helper function to print the banner
+def printWelcomeBannerHelper():
+    print("===================")
+    print("Welcome to the Grade Calculator.")
+    print("===================")
 
 
-if usernameAttempt != username and passwordAttempt != username:
+if usernameAttempt != username or passwordAttempt != password:
     print("You have entered an incorrect username or password. Please try again.")
     exit()
 else:
